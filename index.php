e04f8209de7256ef53e2b274ce38f35e05a373e0
no_name <no@email.com>
2026-07-23 16:27:38 +0100

    remove smartsupp

--- index.php
+++ index.php
@@ -50,18 +50,6 @@ body {
     padding: 6px 25px;
 }
     </style>
-<!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = 'ad38dd34b94c75a4bb1bd53c2fe160a618f05ea7';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
-</script>
-<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 </head>
 
 <body id="top">
